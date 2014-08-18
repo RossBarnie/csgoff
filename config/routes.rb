833ab301bd4fbbaa3b726bbac8503e0d8c1f8bd4
Ross Barnie <ross@tictocfamily.com>
2014-08-18 15:29:58 +0100

    added session controller, logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Csgoff::Application.routes.draw do
 
   root 'static_pages#home'
   get '/about' => 'static_pages#about'
+  get '/logout' => 'session#destroy'
 
-  post "/auth/steam/callback" => "static_pages#callback"
+  post "/auth/steam/callback" => "session#callback"
 
 end
